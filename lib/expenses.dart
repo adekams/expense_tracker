89618941e8184130682f07e201013c86ac4fceba
Adekams <kifademi@gmail.com>
2023-05-13 00:15:48 +0100

    starting expenses

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -8,8 +11,32 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'expense tracker',
+      amount: 10.95,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Popcorn',
+      amount: 2.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,3 @@ void main() {
     home: Expenses(),
   ));
 }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Text('Hello World!'),
-//         ),
-//       ),
-//     );
-//   }
-// }
