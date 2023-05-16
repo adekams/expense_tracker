3287d6365e648771598cc5c0b85176ea5c48cfc0
Adenike-encentral <adenike@encentralsolutions.com>
2023-05-17 00:06:42 +0100

    delete expense by swiping

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,23 @@ import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
+  final Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, indx) => ExpenseItem(expenses[indx]),
+      itemBuilder: (ctx, indx) => Dismissible(
+        key: ValueKey(indx),
+        child: ExpenseItem(expenses[indx]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[indx]);
+        },
+      ),
     );
   }
 }
